0e5c0d4c7d7a3835478df0213ffe6e6e11acc641
Lê Quang Ánh <superanhziggy@gmail.com>
2024-06-23 01:08:31 +0700

    đau đầu vãi

--- src/modules/Admin/TypeofWork/index.tsx
+++ src/modules/Admin/TypeofWork/index.tsx
@@ -1,9 +1,9 @@
-import { TableProps, Tag, Table, Pagination, Button, Col, Form, Input, Row, Popconfirm, Tooltip, Breadcrumb } from "antd";
-import { useState } from "react";
-import { PAGE_SIZE, PAGE_SIZE_TYPEJOB } from "../../../constants";
+import { TableProps,Image , Table, Pagination, Button, Col, Form, Input, Row, Popconfirm, Tooltip, Breadcrumb, Modal, Popover, Tag } from "antd";
+import {  useState } from "react";
+import {  PAGE_SIZE_TYPEJOB } from "../../../constants";
 import { IconButton, Iconify } from "../../../icon";
-import { JobType } from "../../../types/job";
-import { useGetListJobType } from "../../../apis/CallApiMaLoaiCongViec/typejob";
+import { DetailJobType, JobType, ListDetailJobType } from "../../../types/job";
+import {  useDetailTypeJob, useGetListJobType, useListDetailTypeJob } from "../../../apis/CallApiMaLoaiCongViec/typejob";
 
 
 export default function Typework() {
@@ -46,22 +46,124 @@ export default function Typework() {
               />
             </IconButton>
           </Popconfirm>
+          
         </div>
       ),
     },
     
   ];
+  const columnsDt: TableProps<DetailJobType>['columns']=[
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Group Name',
+      dataIndex: 'tenNhom',
+      key: 'tenNhom',
+    },
+    {
+      title: 'Image',
+      dataIndex: 'hinhAnh',
+      key: 'hinhAnh',
+      render: (_,{hinhAnh}, index) => (
+        <Image
+          width={100}
+          src={hinhAnh}
+          alt={`Image ${index}`}
+        />
+      ),
+    },
+    {
+      title: 'Type Job ID',
+      dataIndex: 'maLoaiCongviec',
+      key: 'maLoaiCongviec',
+    },
+    {
+      title: 'Details',
+      dataIndex: 'dsChiTietLoai',
+      key: 'dsChiTietLoai',
+      render: (details: ListDetailJobType[]) => {
+        const content = (
+          <div>
+            {details.map(detail => (
+              <Tooltip key={detail.id} title={`ID: ${detail.id}`}>
+                <Tag color="yellow">Name: {detail.tenChiTiet}</Tag>
+              </Tooltip>
+            ))}
+          </div>
+        );
+
+        return (
+          <Popover mouseEnterDelay={0.5} content={content} title="Details">
+            <Button type="link">Details</Button>
+          </Popover>
+        );
+      },
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      align: 'center',
+      
+      render: () => (
+        <div className="text-gray flex w-full items-center justify-center">
+          <IconButton >
+            <Iconify icon="solar:pen-bold-duotone" size={18} />
+          </IconButton>
+          <Popconfirm
+            title="Delete This Type?"
+            okText="Yes"
+            cancelText="No"
+            placement="left"
+            onConfirm={() => {
+              // submitHandleDelete(record.id.toString());
+            }}
+          >
+            <IconButton>
+              <Iconify
+                icon="mingcute:delete-2-fill"
+                size={18}
+                className="text-error"
+              />
+            </IconButton>
+          </Popconfirm>
+          
+        </div>
+      ),
+    },
+  ]
+  
   const [currentPage,setCurrentPage]=useState(1);
+  
   const {data, isLoading}=useGetListJobType(currentPage);
   const dataSource=data?.data ||[]
   const [form] = Form.useForm();
   const totalCount=data?.totalRow||0;
+  const dataDetailJob=[...dataSource]||[];
+  const [currentPagedetail,setcurrentpageDetail]=useState(1);
+  const {data:typejobdetail,isLoading:loadingtypejobdetail}=useListDetailTypeJob(currentPagedetail)
+  const [isopenModal,setisopenModal]=useState(false);
+  const dataSourceJobDetail=typejobdetail?.data||[];
+  const totalCountdetail=typejobdetail?.totalRow||0;
+  const [detailopenmodal,setDetailOpenModal]=useState(false);
+  const [id,setid]=useState(0);
+  const {data:detailData,isLoading:detailLoad}=useDetailTypeJob()
+  const detaildata=detailData?.filter((item:DetailJobType) => item.maLoaiCongviec === id)||[];
+  
   const onFinishHandler = (values: any) => {
     console.log(values);
   };
   const resetHandler = () => {
     form.resetFields();
   };
+  
+  const handledetailJob = () => {
+    setDetailOpenModal(true);
+    
+  };
+  
   return (
     <>
     <div className="flex items-center justify-between">
@@ -81,7 +183,7 @@ export default function Typework() {
     <div className="mt-3 text-2xl">
     <Form form={form} onFinish={onFinishHandler}>
           <Row gutter={24}  justify="space-between">
-            <Col   xs={12} md={18} sm={14} lg={19} xl={20} xxl={18}>
+            <Col   xs={12} md={18} sm={14} lg={4} xl={20} xxl={18}>
               <Row  gutter={[12,12]}>
                 <Col xs={24} md={21} sm={12}>
                   <Row>
@@ -100,9 +202,12 @@ export default function Typework() {
                 </Col>
               </Row>
             </Col>
-            <Col xs={12} sm={10} md={6} lg={5} xl={4} xxl={6}>
-              <Row>
-                <Col xs={24} sm={12} lg={3}>
+            <Col xs={12} sm={10} md={6} lg={20} xl={4} xxl={6}>
+              <Row gutter={[12,12]}>
+              <Col xs={24} sm={12} lg={24}>
+                  <Button onClick={()=>setisopenModal(true)}  type="primary" danger >List Detail Job</Button>
+                </Col>
+                <Col xs={12} sm={12} lg={24}>
                   <Button type="primary" >Add new Type</Button>
                 </Col>
               </Row>
@@ -117,6 +222,15 @@ export default function Typework() {
       pagination={false}
       bordered
       loading={isLoading}
+      style={{cursor:'pointer'}}
+      onRow={({id}) => {
+        return {
+          onClick: () => {
+            setid(id)
+            handledetailJob();
+          },
+        };
+      }}
       />
       <div className="flex float-end mt-4 pb-4">
           <Pagination
@@ -128,6 +242,59 @@ export default function Typework() {
             }}
           />
         </div>
+        <Modal
+        title="Job Details"
+        centered
+        open={isopenModal}
+        onOk={()=>{setisopenModal(false); }}
+        onCancel={()=>{setisopenModal(false);}}
+        width={1000}
+        
+      >
+        <Table 
+      className="mt-2"
+      columns={columnsDt}
+      rowKey={"id"}
+      dataSource={dataSourceJobDetail}
+      pagination={false}
+      bordered
+      loading={loadingtypejobdetail}
+      
+      
+      />
+      <div className="mt-2">
+          <Pagination
+            defaultCurrent={currentPagedetail} 
+            
+            total={totalCountdetail} 
+            pageSize={PAGE_SIZE_TYPEJOB}
+            onChange={(page: number) => {
+              setcurrentpageDetail(page);
+            }}
+          />
+        </div>
+      </Modal>
+      <Modal
+        title="Job Details"
+        centered
+        open={detailopenmodal}
+        onOk={()=>setDetailOpenModal(false)}
+        onCancel={()=>setDetailOpenModal(false)}
+        width={1000}
+      >
+        <Table 
+      className="mt-2"
+      columns={columnsDt}
+      rowKey={"id"}
+      dataSource={detaildata}
+      pagination={false}
+      bordered
+      loading={detailLoad}
+      
+      
+      />
+      
+      </Modal>
     </div>
     </>
   )
--- src/modules/Admin/Work/index.tsx
+++ src/modules/Admin/Work/index.tsx
@@ -1,90 +1,145 @@
-import { TableProps, Tag, Table, Pagination, Button, Col, Form, Input, Row, Popconfirm, Tooltip, Breadcrumb } from "antd";
+import {
+  TableProps,
+  Table,
+  Pagination,
+  Button,
+  Col,
+  Form,
+  Input,
+  Row,
+  Popconfirm,
+  Tooltip,
+  Breadcrumb,
+  Modal,
+  Upload,
+} from "antd";
 import { useState } from "react";
 import { PAGE_SIZE } from "../../../constants";
 import { IconButton, Iconify } from "../../../icon";
-import { useGetListJob } from "../../../apis/CallApiCongViec/job";
+import { useAddJob, useDeleteJob, useGetListJob, useUpdateJob } from "../../../apis/CallApiCongViec/job";
 import { Career } from "../../../types/job";
-
+import { Controller, useForm } from "react-hook-form";
+import { UploadOutlined, DeleteOutlined } from "@ant-design/icons";
 
 export default function Work() {
-  
-  const hinhAnh='https://t4.ftcdn.net/jpg/01/24/65/69/360_F_124656969_x3y8YVzvrqFZyv3YLWNo6PJaC88SYxqM.jpg'
-  const columns: TableProps<Career>['columns'] = [
+  const { handleSubmit, control, watch, setValue, reset } = useForm<Career>({
+    defaultValues: {
+      id: 0,
+      tenCongViec: "",
+      danhGia: 0,
+      giaTien: 0,
+      nguoiTao: 0,
+      hinhAnh: "",
+      moTa: "",
+      maChiTietLoaiCongViec: 0,
+      moTaNgan: "",
+      saoCongViec: 0,
+    },
+  });
+  const hinhAnh =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbUxUTYzeP71ankRCJ2bhiC7ZXN_H7sjvd1g&s";
+  const columns: TableProps<Career>["columns"] = [
     {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
     },
     {
-      title: 'Image',
-      dataIndex: 'hinhAnh',
-      key: 'hinhAnh',
-      render: (key:string) => (
+      title: "Image",
+      dataIndex: "hinhAnh",
+      key: "hinhAnh",
+      render: (key: string) => (
         <div>
-          <img className="w-[80px] h-[80px] rounded object-cover" src={key===""?hinhAnh:key} alt="avatar" />
+          <img
+            className="w-[80px] h-[80px] rounded object-cover"
+            src={key === "" ? hinhAnh : key}
+            alt="avatar"
+          />
         </div>
       ),
     },
     {
-      title: 'Job name',
-      dataIndex: 'tenCongViec',
-      key: 'tenCongViec',
-      render: text => (
+      title: "Job name",
+      dataIndex: "tenCongViec",
+      key: "tenCongViec",
+      render: (text) => (
         <Tooltip title={text}>
-          <div style={{ maxWidth: '300px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+          <div
+            style={{
+              maxWidth: "300px",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
             {text}
           </div>
         </Tooltip>
       ),
     },
     {
-      title: 'Price',
-      dataIndex: 'giaTien',
-      key: 'giaTien',
-      
+      title: "Price",
+      dataIndex: "giaTien",
+      key: "giaTien",
     },
     {
-      title: 'Creator',
-      dataIndex: 'nguoiTao',
-      key: 'nguoiTao',
+      title: "Creator",
+      dataIndex: "nguoiTao",
+      key: "nguoiTao",
     },
     {
-      title: 'ID Details job',
-      dataIndex: 'maChiTietLoaiCongViec',
-      key: 'maChiTietLoaiCongViec',
+      title: "ID Details job",
+      dataIndex: "maChiTietLoaiCongViec",
+      key: "maChiTietLoaiCongViec",
     },
     {
-      title: 'Description',
-      dataIndex: 'moTa',
-      key: 'moTa',
-      render: text => (
+      title: "Description",
+      dataIndex: "moTa",
+      key: "moTa",
+      render: (text) => (
         <Tooltip title={text}>
           <span>{text.length > 3 ? `${text.substring(0, 3)}...` : text}</span>
         </Tooltip>
       ),
     },
     {
-      title: 'Star',
-      key: 'saoCongViec',
-      dataIndex: 'saoCongViec',
-      align: 'center',
+      title: "Star",
+      key: "saoCongViec",
+      dataIndex: "saoCongViec",
+      align: "center",
       render: (_, { saoCongViec }) => (
         <>
           {Array.from({ length: saoCongViec }, (_, index) => (
-            <span key={index} style={{ color: '#ffcc00', fontSize: '16px' }}>★</span>
+            <span key={index} style={{ color: "#ffcc00", fontSize: "16px" }}>
+              ★
+            </span>
           ))}
         </>
       ),
     },
-    
+
     {
-      title: 'Action',
-      key: 'action',
-      align: 'center',
-      
-      render: () => (
+      title: "Action",
+      key: "action",
+      align: "center",
+
+      render: (_,{id,tenCongViec,danhGia,giaTien,nguoiTao,saoCongViec,moTa,hinhAnh,moTaNgan,maChiTietLoaiCongViec}) => (
         <div className="text-gray flex w-full items-center justify-center">
-          <IconButton >
+          <IconButton onClick={()=>{
+            setisopenModal(true); 
+            setisupdate(true);
+            setValue("id",id);
+            setValue("tenCongViec",tenCongViec);
+            setValue("danhGia",danhGia);
+            setValue("giaTien",giaTien);
+            setValue("nguoiTao",nguoiTao);
+            setValue("moTa",moTa);
+            setValue("moTaNgan",moTaNgan);
+            setValue("hinhAnh",hinhAnh);
+            setValue("saoCongViec",saoCongViec);
+            setValue("maChiTietLoaiCongViec",maChiTietLoaiCongViec);
+
+          }}>
             <Iconify icon="solar:pen-bold-duotone" size={18} />
           </IconButton>
           <Popconfirm
@@ -93,7 +148,7 @@ export default function Work() {
             cancelText="No"
             placement="left"
             onConfirm={() => {
-              // submitHandleDelete(record.id.toString());
+              submitHandleDelete(id);
             }}
           >
             <IconButton>
@@ -108,20 +163,63 @@ export default function Work() {
       ),
     },
   ];
-  const [currentPage,setCurrentPage]=useState(1);
-  const {data, isLoading}=useGetListJob(currentPage);
-  const dataSource=data?.data ||[]
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data, isLoading } = useGetListJob(currentPage);
+  const dataSource = data?.data || [];
+  const [isopenModal, setisopenModal] = useState(false);
+  const [isupDate, setisupdate] = useState(false);
   const [form] = Form.useForm();
-  const totalCount=data?.totalRow||0;
+  const totalCount = data?.totalRow || 0;
+  const {mutate:addNewJob,isPending}=useAddJob(currentPage,()=>setisopenModal(false),reset)
+  const {mutate: deleteJob}=useDeleteJob(currentPage);
+  const {mutate:updateJob}=useUpdateJob(currentPage,()=>setisopenModal(false),reset)
+  const hinhAnhValue = watch("hinhAnh");
+
+  
   const onFinishHandler = (values: any) => {
     console.log(values);
   };
+  const submitHandleDelete=(id:any)=>{
+    deleteJob(id);
+  }
+  const handleUpdateJob=(form:any)=>{
+    updateJob({ job: form, values: form.id });
+  }
   const resetHandler = () => {
     form.resetFields();
   };
+  const handleAddJob=(form:Career)=>{
+    addNewJob(form)
+  }
+  const onsubmit = (formValue: Career) => {
+    if(!isupDate){
+      const form={
+        id: 0,
+        tenCongViec: formValue.tenCongViec,
+        danhGia: formValue.danhGia,
+        giaTien: formValue.giaTien,
+        nguoiTao: formValue.nguoiTao,
+        hinhAnh: formValue.hinhAnh ? String(formValue.hinhAnh) : "",
+        moTa: formValue.moTa,
+        maChiTietLoaiCongViec: formValue.maChiTietLoaiCongViec,
+        moTaNgan: formValue.moTaNgan,
+        saoCongViec: formValue.saoCongViec,
+      }
+      handleAddJob(form);
+    }
+    else{
+      handleUpdateJob(formValue);
+    }
+  };
+  const handleOpenModalForAdd = () => {
+    setisopenModal(true);
+    setisupdate(false);
+    // Reset form fields for adding new job
+    reset();
+  };
   return (
     <>
-    <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between">
         <Breadcrumb
           items={[
             {
@@ -133,23 +231,22 @@ export default function Work() {
             },
           ]}
         />
-        
-    </div>
-    <div className="mt-3 text-2xl">
-    <Form form={form} onFinish={onFinishHandler}>
-          <Row gutter={24}  justify="space-between">
-            <Col   xs={12} md={18} sm={14} lg={19} xl={20} xxl={18}>
-              <Row  gutter={[12,12]}>
+      </div>
+      <div className="mt-3 text-2xl">
+        <Form form={form} onFinish={onFinishHandler}>
+          <Row gutter={24} justify="space-between">
+            <Col xs={12} md={18} sm={14} lg={19} xl={20} xxl={18}>
+              <Row gutter={[12, 12]}>
                 <Col xs={24} md={21} sm={12}>
                   <Row>
-                  <Col  xs={23} md={21} sm={24} lg={8}>
-                  <Form.Item name="Search">
-                    <Input placeholder="Search by name" allowClear />
-                  </Form.Item>
-                  </Col>
-                  
-                    <Col xs={24} md={3} sm={24} lg={16} >
-                      <Button  type="primary" onClick={resetHandler}>
+                    <Col xs={23} md={21} sm={24} lg={8}>
+                      <Form.Item name="Search">
+                        <Input placeholder="Search by name" allowClear />
+                      </Form.Item>
+                    </Col>
+
+                    <Col xs={24} md={3} sm={24} lg={16}>
+                      <Button type="primary" onClick={resetHandler}>
                         Reset
                       </Button>
                     </Col>
@@ -160,31 +257,259 @@ export default function Work() {
             <Col xs={12} sm={10} md={6} lg={5} xl={4} xxl={6}>
               <Row>
                 <Col xs={24} sm={12} lg={3}>
-                  <Button type="primary" >Add new Job</Button>
+                  <Button
+                    type="primary"
+                    onClick={() => {
+                      handleOpenModalForAdd();
+                    }}
+                  >
+                    Add new Job
+                  </Button>
                 </Col>
               </Row>
             </Col>
           </Row>
         </Form>
-      <Table 
-      className="mt-2"
-      columns={columns}
-      rowKey={"id"}
-      dataSource={dataSource}
-      pagination={false}
-      loading={isLoading}
-      />
-      <div className="flex float-end mt-4 pb-4">
+        <Table
+          className="mt-2"
+          columns={columns}
+          rowKey={"id"}
+          dataSource={dataSource}
+          pagination={false}
+          loading={isLoading}
+        />
+        <div className="flex float-end mt-4 pb-4">
           <Pagination
-            defaultCurrent={currentPage} 
-            total={totalCount} 
+            defaultCurrent={currentPage}
+            total={totalCount}
             pageSize={PAGE_SIZE}
             onChange={(page: number) => {
               setCurrentPage(page);
             }}
           />
         </div>
-    </div>
+        <Modal
+          title={isupDate ? "Cập nhật Công Việc " : "Thêm Công việc"}
+          centered
+          open={isopenModal}
+          onClose={() => setisupdate(false)}
+          onCancel={() => setisopenModal(false)}
+          footer={false}
+        >
+          <Form className="mt-2" onFinish={handleSubmit(onsubmit)}>
+            <Row gutter={[18, 18]}>
+              {isupDate && (
+                <Col span={24}>
+                  <label className="text-sm" htmlFor="">
+                    ID
+                  </label>
+                  <Controller
+                    name="id"
+                    control={control}
+                    render={({ field }) => (
+                      <Input
+                        size="large"
+                        className="mt-1"
+                        disabled
+                        {...field}
+                      />
+                    )}
+                  />
+                </Col>
+              )}
+              <Col span={24}>
+                <label className="text-sm" htmlFor="">
+                  Tên Công Việc
+                </label>
+                <Controller
+                  name="tenCongViec"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      size="large"
+                      className="mt-1"
+                      placeholder="Tên công Việc"
+                      {...field}
+                    />
+                  )}
+                />
+              </Col>
+              <Col span={24}>
+                <label className="text-sm" htmlFor="">
+                  Đánh giá
+                </label>
+                <Controller
+                  name="danhGia"
+                  control={control}
+                  render={({ field }) => {
+                    return (
+                      <Input
+                        type="number"
+                        size="large"
+                        className="mt-1"
+                        placeholder="Đánh Giá..."
+                        {...field}
+                      />
+                    );
+                  }}
+                />
+              </Col>
+              <Col span={24}>
+                <label className="text-sm" htmlFor="">
+                  Giá tiền
+                </label>
+                <Controller
+                  name="giaTien"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      type="number"
+                      size="large"
+                      className="mt-1"
+                      placeholder="Price..."
+                      {...field}
+                    />
+                  )}
+                />
+              </Col>
+
+              <Col span={24}>
+                <label className="text-sm" htmlFor="">
+                  Người Tạo
+                </label>
+                <Controller
+                  control={control}
+                  name="nguoiTao"
+                  render={({ field }) => (
+                    <Input
+                      type="number"
+                      size="large"
+                      className="mt-1"
+                      placeholder="Number..."
+                      {...field}
+                    />
+                  )}
+                />
+              </Col>
+              <Col span={12}>
+                <label className="text-sm" htmlFor="">
+                  Mã Loại Công Việc
+                </label>
+                <Controller
+                  name="maChiTietLoaiCongViec"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      size="large"
+                      className="mt-1"
+                      placeholder="Id..."
+                      {...field}
+                    />
+                  )}
+                />
+              </Col>
+              <Col span={12}>
+                <label className="text-sm" htmlFor="">
+                  Sao Công Việc
+                </label>
+                <Controller
+                  control={control}
+                  name="saoCongViec"
+                  render={({ field }) => (
+                    <Input
+                      type="number"
+                      size="large"
+                      className="mt-1"
+                      placeholder="Number..."
+                      {...field}
+                    />
+                  )}
+                />
+              </Col>
+              <Col span={24}>
+                <label className="text-sm" htmlFor="">
+                  Short Description
+                </label>
+                <Controller
+                  name="moTaNgan"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      size="large"
+                      className="mt-1"
+                      placeholder="mô tả ngắn..."
+                      {...field}
+                    />
+                  )}
+                />
+              </Col>
+              <Col span={24}>
+                <label className="text-sm" htmlFor="">
+                  Long Description
+                </label>
+                <Controller
+                  name="moTa"
+                  control={control}
+                  render={({ field }) => (
+                    <Input.TextArea
+                      rows={4}
+                      size="small"
+                      className="mt-1"
+                      placeholder="mô tả dài..."
+                      {...field}
+                    />
+                  )}
+                />
+              </Col>
+              <Col span={24}>
+                <label className="text-sm" htmlFor="">
+                  Hình ảnh
+                </label>
+                <Controller
+                  name="hinhAnh"
+                  control={control}
+                  render={({ field }) => {
+                    return(
+                      <Input
+                      size="large"
+                      className="mt-1"
+                      placeholder="Nhập đường dẫn ảnh"
+                      {...field}
+                    />
+                    )
+                  }}
+                />
+                {hinhAnhValue && typeof hinhAnhValue === "string" && (
+                  <div className="mt-2">
+                    <img
+                      src={hinhAnhValue}
+                      className="w-[100px] h-[100px] object-cover rounded"
+                    />
+                    <span
+                      className="inline-block ml-3 cursor-pointer"
+                      onClick={() => setValue("hinhAnh", undefined)}
+                    >
+                      <DeleteOutlined />
+                    </span>
+                  </div>
+                )}
+              </Col>
+
+              <Col span={24} className="text-end">
+                <Button
+                  loading={isPending}
+                  disabled={isPending}
+                  htmlType="submit"
+                  size="large"
+                  type="primary"
+                >
+                  {isupDate ? "Update" : "Add new"}
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Modal>
+      </div>
     </>
-  )
+  );
 }
